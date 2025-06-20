6d587f30f1da2f424c6d5f42ad8db68f832103c3
Miles Larkot <154857772+MilesLarkot@users.noreply.github.com>
2025-06-20 08:56:03 +0000

    url detection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1>Env Check</h1>
+      <p>VERCEL_URL: {process.env.VERCEL_URL}</p>
+      <p>NODE_ENV: {process.env.NODE_ENV}</p>
+    </div>
+  );
 }
