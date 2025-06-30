8558dfeb17e7dd16db17fe40a602a1fe56cc256f
Miles Larkot <biteblight@proton.me>
2025-06-30 11:15:52 +0000

    404 mascot

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFound from "@/components/NotFound";
+
+function NotFoundPage() {
+  return (
+    <div className="h-svh flex items-center justify-center bg-black">
+      <NotFound size="500px" className="animate-float" />
+    </div>
+  );
+}
+
+export default NotFoundPage;
